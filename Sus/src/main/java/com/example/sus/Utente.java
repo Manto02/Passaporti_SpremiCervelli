cdb4d213b925e8c2f9ab4efcba59091b834422bd
schioppi <92445305+schioppi@users.noreply.github.com>
2023-08-07 16:50:06 +0200

    Add files via upload

---
+++ Sus/src/main/java/com/example/sus/Utente.java
@@ -0,0 +1,13 @@
+package com.example.sus;
+
+import java.util.Date;
+
+public class Utente {
+
+    private static Date dayOfBirth;
+    private static String name;
+    public Utente(String name, Date dayOfBirth){
+
+    }
+
+}
---
+++ Sus/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.sus {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.sus to javafx.fxml;
+    exports com.example.sus;
+}
