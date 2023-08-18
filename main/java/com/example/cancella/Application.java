5bfbeca654c55f1abeea506a39861b72ac77de81
CrocseRossa <76122737+CrocseRossa@users.noreply.github.com>
2023-08-18 11:57:42 +0200

    calendario figo
    sono molto fiero di questo calendario

---
+++ main/java/com/example/cancella/Application.java
@@ -0,0 +1,23 @@
+package com.example.cancella;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        // "CustomDatePicker.fxml"
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("designCarino.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
